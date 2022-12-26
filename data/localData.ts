2004a15a12bec000904111f3ff99bc5c7ab84c9b
bravelasagna <85801832+bravelasagna@users.noreply.github.com>
2022-12-26 19:00:13 +0100

    3 files modified, 9 deleted

--- data/localData.ts
+++ data/localData.ts
@@ -3,40 +3,35 @@ import { TaskEntity } from './taskEntity';
 
 // local data records for the Project Entity.
 // A Project is a serie of Tasks that need to be accomplished to reach a specific goal.
-export default class LocalData {
-  returnLocalDataProjects() {
-    const dbProjectsList: ProjectEntity[] = [
+export const LocalData = [
+  {
+    projectId: 1,
+    title: 'Famiglia',
+    tasks: [
       {
+        taskId: '1-1',
         projectId: 1,
-        title: 'Famiglia',
-        tasks: [
-          {
-            taskId: '1-1',
-            projectId: 1,
-            title: 'Andare in lavanderia',
-            description: 'Author 1',
-          },
-          {
-            taskId: '1-2',
-            projectId: 1,
-            title: 'Pagare F24',
-            description: 'Author 1',
-          },
-        ],
+        title: 'Andare in lavanderia',
+        description: 'Author 1',
       },
       {
-        projectId: 2,
-        title: 'Formazione',
-        tasks: [
-          {
-            taskId: '2-1',
-            projectId: 2,
-            title: 'Finire corso React',
-            description: 'Author 1',
-          },
-        ],
+        taskId: '1-2',
+        projectId: 1,
+        title: 'Pagare F24',
+        description: 'Author 1',
       },
-    ];
-    return dbProjectsList;
-  }
-}
+    ],
+  },
+  {
+    projectId: 2,
+    title: 'Formazione',
+    tasks: [
+      {
+        taskId: '2-1',
+        projectId: 2,
+        title: 'Finire corso React',
+        description: 'Author 1',
+      },
+    ],
+  },
+];
--- index.tsx
+++ index.tsx
@@ -1,19 +1,44 @@
-// https://github.com/mkaya13/react-todo-list
-// https://uizard.io/templates/web-app-templates/to-do-web-app/
-
 import * as React from 'react';
-import { StrictMode } from 'react';
+import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//import ToDoList from './components/To
-import Dashboard from './components/Dashboard';
+import { LocalData } from './data/localData';
+import { ProjectEntity } from './data/projectEntity';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+// MAIN APP
+function App() {
+  // INIT LOCAL DATA
+  const [dataListProjectsState, setDataListProjectsState] = useState(LocalData);
+
+  return (
+    <div>
+      <ListProjects dataListProjects={dataListProjectsState}></ListProjects>
+    </div>
+  );
+}
+
+function ListProjects(dataListProjects) {
+  console.log(dataListProjects);
+
+  return (
+    <div>
+      {dataListProjects.map((project) => {
+        return (
+          <div>
+            <li key={project.projectId}>{project.title}</li>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+// MAIN RENDERING
 root.render(
   <StrictMode>
-    <Dashboard />
+    <App></App>
   </StrictMode>
 );
