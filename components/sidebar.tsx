550355235860ed77a8a8a9ff6bd254ef9d8dc490
bravelasagna <85801832+bravelasagna@users.noreply.github.com>
2022-12-29 16:30:46 +0100

    1 file modified

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,16 +4,29 @@ import { Link } from 'react-router-dom';
 
 export default function SideBar() {
   let menuItems: SideBarMenuItem[] = [];
-  menuItems.push({ to: '/homepage', caption: 'Homepage' });
-  menuItems.push({ to: '/colleagues', caption: 'Colleagues' });
+  menuItems.push({
+    to: '/homepage',
+    caption: 'Homepage',
+    icon: 'calendar.png',
+  });
+  menuItems.push({
+    to: '/colleagues',
+    caption: 'Colleagues',
+    icon: 'people.png',
+  });
+
+  let tempPathAssets =
+    'https://raw.githubusercontent.com/bravelasagna/Test-template/main/assets/';
 
   return (
     <nav className="flex-column">
       {menuItems.map((menu) => (
-        <Link to={menu.to} className="nav-link">
-          <img className="menu-icon" src={menu.icon} />
-          {menu.caption}
-        </Link>
+        <div className="nav-item px-3">
+          <Link to={menu.to} className="nav-link">
+            <img className="menu-icon" src={tempPathAssets + menu.icon} />
+            {menu.caption}
+          </Link>
+        </div>
       ))}
     </nav>
   );
