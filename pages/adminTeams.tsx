33ac4ccdf76c7600e4a082ff463ee9046ab43ecc
bravelasagna <85801832+bravelasagna@users.noreply.github.com>
2022-12-30 15:30:13 +0100

    1 file modified, 1 added

--- pages/adminTeams.tsx
+++ pages/adminTeams.tsx
@@ -1,52 +1,68 @@
 import { StrictMode, useState, useEffect, useCallback } from 'react';
 import React = require('react');
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ReactDataGrid from '@inovua/reactdatagrid-community';
 import '@inovua/reactdatagrid-community/index.css';
 
-import { LocalData } from '../localData';
+import LinkButton from './../ui/linkButton';
+import ListTasks from './../components/tasksList';
 
 import { TeamEntity } from './../models/teamEntity';
 
-export default function PagesAdminTeams(propUser) {
+export default function PagesAdminTeams() {
+  // STATE OBJECTS
   const [showPanel, setShowPanel] = useState('list');
   const [teamsList, setTeamsList] = useState(new Array<TeamEntity>());
+  const [currentTeamId, setCurrentTeamId] = useState(0);
   const [txtTeamNameValueState, setTxtTeamNameValueState] = useState('');
 
+  // DATAGRID CONFIG
   const columns = [
     { name: 'teamId', header: 'TeamId', minWidth: 50, defaultFlex: 2 },
     { name: 'teamName', header: 'Name', minWidth: 50, defaultFlex: 2 },
   ];
 
+  // ON LOAD
   useEffect(() => {
-    setTeamsList(LocalData.Teams);
-    console.log('use effect was called');
-
     fetch('https://mockend.com/bravelasagna/test-template/Teams?limit=5')
       .then((res) => res.json())
       .then(
         (result) => {
           console.log(result);
+          setTeamsList(result);
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {}
       );
   }, []);
 
+  // LIST PANEL
   const onRowClick = useCallback((rowProps, event) => {
-    console.log(rowProps);
     setShowPanel('edit');
     setTxtTeamNameValueState(rowProps.data.teamName);
+    setCurrentTeamId(rowProps.data.teamId);
   }, []);
 
+  function handleAddClick() {
+    setShowPanel('edit');
+    setTxtTeamNameValueState('');
+    setCurrentTeamId(0);
+  }
+
+  // EDIT PANEL
   function handleCancelClick() {
     setShowPanel('list');
   }
 
   function handleOnChange(e) {
     setTxtTeamNameValueState(e.target.value);
+  }
+
+  function handleDeleteClick() {
+    setShowPanel('list');
+  }
+
+  function handleSaveClick() {
+    setShowPanel('list');
   }
 
   function RenderValidation() {
@@ -71,7 +87,8 @@ export default function PagesAdminTeams(propUser) {
         <br />
         <br />
         <br />
-        <Link to={'/admin'}>torna pannello di controllo</Link>
+        <LinkButton to="/admin">Torna Indietro</LinkButton>
+        <button onClick={(e) => handleAddClick()}>Crea nuovo Team</button>
       </div>
       <div hidden={showPanel != 'edit'}>
         DETTAGLI TEAM
@@ -84,7 +101,9 @@ export default function PagesAdminTeams(propUser) {
         <RenderValidation></RenderValidation>
         <br />
         <br />
-        <a onClick={(e) => handleCancelClick()}>Torna alla lista</a>
+        <button onClick={(e) => handleCancelClick()}>Torna alla lista</button>
+        <button onClick={(e) => handleDeleteClick()}>Delete</button>
+        <button onClick={(e) => handleSaveClick()}>Save</button>
       </div>
     </div>
   );
