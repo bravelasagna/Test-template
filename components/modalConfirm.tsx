cec389e0af11705b963d706ecee049dc3073c47b
bravelasagna <85801832+bravelasagna@users.noreply.github.com>
2023-01-02 21:28:21 +0100

    1 file modified

--- components/modalConfirm.tsx
+++ components/modalConfirm.tsx
@@ -3,15 +3,18 @@ import React = require('react');
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-export default function ModalConfirm(showParam) {
+export default function ModalConfirm(params) {
+  // params.show -> display the delete modal
+  // params.body -> message in the body of the confirm modal
+
   return (
     <div>
-      <Modal show={showParam}>
+      <Modal show={params.show} onHide={params.onClose} animation={false}>
         <Modal.Header closeButton>
           <Modal.Title>Confirm</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>kjlhljh</Modal.Body>
+        <Modal.Body>{params.body}</Modal.Body>
 
         <Modal.Footer>
           <Button variant="secondary">No</Button>
